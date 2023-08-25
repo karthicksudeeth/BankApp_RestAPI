2bc40c6c1eda4395c4823e700fab4637943e39a0
karthicksudeeth <112969229+karthicksudeeth@users.noreply.github.com>
2023-08-25 16:25:56 +0530

    Update MockBankDataSource.kt

--- src/main/kotlin/com/practice/simpleRest/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/com/practice/simpleRest/datasource/mock/MockBankDataSource.kt
@@ -1,11 +1,9 @@
 package com.practice.simpleRest.datasource.mock
-
 import com.practice.simpleRest.datasource.BankDataSource
 import com.practice.simpleRest.model.Bank
 import org.springframework.stereotype.Repository
 import java.lang.IllegalArgumentException
 import java.lang.NullPointerException
-
 @Repository
 class MockBankDataSource: BankDataSource {
     val banks= mutableListOf(
@@ -13,12 +11,10 @@ class MockBankDataSource: BankDataSource {
             Bank("45678", 0.0, 6),
             Bank("12345", 0.0, 2)
     )
-
     override fun getBanks(): Collection<Bank> = banks
     override fun getBank(accountNumber: String): Bank =
             banks.firstOrNull(){it.accountNumber == accountNumber}
                     ?: throw NoSuchElementException("could not find a bank with the account number $accountNumber")
-
     override fun addBank(bank: Bank): Bank {
         if(banks.any{it.accountNumber==bank.accountNumber}){
             throw IllegalArgumentException("Bank with account number ${bank.accountNumber } already exist")
@@ -26,21 +22,16 @@ class MockBankDataSource: BankDataSource {
        banks.add(bank)
         return bank
     }
-
     override fun updateBank(bank: Bank): Bank {
         val currentBank=banks.firstOrNull(){it.accountNumber==bank.accountNumber}
                 ?:throw IllegalArgumentException("Could not find a bank with the the account number ${bank.accountNumber}")
-
         banks.remove(currentBank)
         banks.add(bank)
         return bank;
     }
-
     override fun deleteAccount(accountNumber: String) {
         val bank=banks.firstOrNull(){it.accountNumber==accountNumber}
                 ?:throw NoSuchElementException("Could not find a bank with the gien account number $accountNumber")
         banks.remove(bank)
     }
-
-
 }
