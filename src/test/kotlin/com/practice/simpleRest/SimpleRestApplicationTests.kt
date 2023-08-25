18d3984f4bf503868188dffcc8988410461c4ded
karthicksudeeth <112969229+karthicksudeeth@users.noreply.github.com>
2023-08-25 16:23:54 +0530

    Update SimpleRestApplicationTests.kt

--- src/test/kotlin/com/practice/simpleRest/SimpleRestApplicationTests.kt
+++ src/test/kotlin/com/practice/simpleRest/SimpleRestApplicationTests.kt
@@ -1,13 +1,9 @@
 package com.practice.simpleRest
-
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-
 @SpringBootTest
 class SimpleRestApplicationTests {
-
 	@Test
 	fun contextLoads() {
 	}
-
 }
