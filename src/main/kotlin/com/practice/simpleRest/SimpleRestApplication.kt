55f4e9c70359466aadfb618874d90304adb18e7c
karthicksudeeth <112969229+karthicksudeeth@users.noreply.github.com>
2023-08-25 16:27:13 +0530

    Update SimpleRestApplication.kt

--- src/main/kotlin/com/practice/simpleRest/SimpleRestApplication.kt
+++ src/main/kotlin/com/practice/simpleRest/SimpleRestApplication.kt
@@ -1,11 +1,8 @@
 package com.practice.simpleRest
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
 @SpringBootApplication
 class SimpleRestApplication
-
 fun main(args: Array<String>) {
 	runApplication<SimpleRestApplication>(*args)
 }
